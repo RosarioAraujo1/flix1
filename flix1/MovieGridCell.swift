ed81c8962b73cb40f5b90366b9b519d895f57f91
Rosario <Rosario@Rosario-MacBook-Pro.local>
2020-02-11 22:42:50 -0800

    flix2

---
+++ flix1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix1
+//
+//  Created by Rosario on 2/9/20.
+//  Copyright © 2020 Rosario. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
